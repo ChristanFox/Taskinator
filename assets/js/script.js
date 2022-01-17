43f9724dd04ebb04d722792bb82037bbf5883ef9
christanfox <bayshorestinks@aol.com>
2022-01-17 11:46:58 -0500

    finished the header

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,22 @@
  /*----Add's Function to the "Add Task" button in the header----*/
 /***************************************************************/
 
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
+
+    event.preventDefault(); // Prevents code from deleting the event and refreshing the page.
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
   }  
 
-buttonEl.addEventListener("click", createTaskHandler);
+  formEl.addEventListener("submit", createTaskHandler);
+  
 
-
+  /***************************************************************/
+ /*----Add's Function to the "Add Task" button in the header----*/
+/***************************************************************/
